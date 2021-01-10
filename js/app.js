05096f5e27b499b4bb0015457169a4b736a29e24
CristianQuito <77211397+CristianQuito@users.noreply.github.com>
2021-01-10 15:45:34 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,106 @@
+// Navegation Menu
+let btnMenu = document.querySelector('.btn-menu');
+let barIconX = document.querySelector('.btn-menu i');
+let menu = document.querySelector('.list-container');
+let menuContent = document.querySelector('.menu');
+var activador = true;
+
+btnMenu.addEventListener('click', (event) => {
+
+  //Icon X
+  barIconX.classList.toggle('fa-times');
+
+   if(activador){
+     menu.style.left = '0%'; 
+     menu.style.transition = '0.5s';
+  
+     activador = false;
+   }
+   else{
+    activador = false;
+    menu.style.left = '-100%';
+
+    activador = true;
+   }
+
+});
+
+// Add class "active"
+let enlaces = document.querySelectorAll('.lists li a');
+
+enlaces.forEach((element) => {
+   
+  element.addEventListener('click', (event) => {
+   enlaces.forEach((link) => {
+     link.classList.remove('active');
+   });
+    event.target.classList.add('active');
+
+  });
+
+});
+
+//Scroll Efect
+
+ let prevScrollPos = window.pageYOffset;
+ let goTop = document.querySelector('.go-top');
+
+window.onscroll = () => {
+  
+  //Hide & Show - Scroll Menu (Function)
+  let currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    menuContent.style.top = '0px';
+    menuContent.style.transition = '0.5s';
+  }else{
+    menuContent.style.top = '-60px';
+    menuContent.style.transition = '0.5s';
+  }
+  prevScrollPos = currentScrollPos;
+  
+  //Scoll Menu & Go Top & See Down (Styles)
+  let arriba = window.pageYOffset;
+
+  //Conditions
+  if(arriba <= 600){
+    menuContent.style.borderBottom = 'none';
+
+    //Ocultar Go Top
+    goTop.style.right = '-100px';
+  }else{
+    menuContent.style.borderBottom = '3px solid #ff2e63';
+
+    //Mostrar Go Top
+    goTop.style.right = '0px';
+  }
+  
+}
+
+//Go Top Click
+document.getElementById("button-up") .addEventListener("click" , scrollUp);
+function scrollUp(){
+    var currentScroll = document.documentElement.scrollTop;
+    
+    if(currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo(0,currentScroll -(currentScroll/10));
+    }
+}
+
+buttonUp = document.getElementById("button-up");
+
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 500){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 500){
+        buttonUp.style.transform = "scale(0)";
+    }
+
+}
+
+
+
